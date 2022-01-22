487f4f4112707da172e9b6c9699c7992fe3b919e
Dan Crank <danno@danno.org>
2022-01-22 13:40:56 -0600

    Initial version

--- ground_control.go
+++ ground_control.go
@@ -1,40 +1,90 @@
 package main
 
 import (
+	"image"
 	"log"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
+	"periph.io/x/conn/v3/i2c/i2creg"
+	"periph.io/x/devices/v3/ssd1306"
+	"periph.io/x/devices/v3/ssd1306/image1bit"
+	"periph.io/x/periph/host"
 )
 
-const telemetryTimeout time.Duration = 10000000 // 10 seconds
+const telemetryTimeout time.Duration = 10 * time.Second  // time to wait for a telemetry message from the rover
+const ackTimeout time.Duration = 1000 * time.Millisecond // time to wait for an ack message from the rover
+const msgDelay time.Duration = 100 * time.Millisecond    // time to wait between Rx and Tx, to give the other side time to switch from Tx to Rx
+
+func initHost() {
+	// Make sure periph is initialized.
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func testDisplay() {
+	// Use i2creg I²C bus registry to find the first available I²C bus.
+	b, err := i2creg.Open("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer b.Close()
+
+	dev, err := ssd1306.NewI2C(b, &ssd1306.DefaultOpts)
+	if err != nil {
+		log.Fatalf("failed to initialize ssd1306: %v", err)
+	}
+
+	// Draw on it.
+	img := image1bit.NewVerticalLSB(dev.Bounds())
+	f := basicfont.Face7x13
+	drawer := font.Drawer{
+		Dst:  img,
+		Src:  &image.Uniform{image1bit.On},
+		Face: f,
+		Dot:  fixed.P(0, img.Bounds().Dy()-1-f.Descent),
+	}
+	drawer.DrawString("Hello from periph!")
+	// if err := dev.Draw(dev.Bounds(), img, image.Point{}); err != nil {
+	// 	log.Fatal(err)
+	// }
+}
 
 func main() {
+	initHost()
+	testDisplay()
 	r := initRadio()
 	// main loop - receive telemetry packets
 	for {
-		msgType, buf, rssi, error := receiveMessage(*r, telemetryTimeout)
-		if error != nil {
-			log.Fatal(r.Error())
+		msgType, buf, rssi, err := receiveMessage(r, telemetryTimeout)
+		if err != nil {
+			log.Fatal(err)
 		}
 		switch msgType {
 		case messageTypeTelemetry:
 			log.Print("Received TELEMETRY message:")
 			log.Printf("RSSI: %d", rssi)
-			tm, error := receiveTelemetryMessage(buf)
+			tm, err := receiveTelemetryMessage(buf)
 			spew.Dump(tm)
-			if error != nil {
-				log.Fatal(r.Error())
+			if err != nil {
+				log.Fatal(err)
 			}
+			// brief pause to give the otehr side time to switch from Tx to Rx
+			time.Sleep(msgDelay)
 			// send the ack
 			ta := telemetryAck{
 				timestamp:      currentTimestamp(),
 				ack:            true,
 				commandWaiting: false,
 			}
-			error = sendMessage(*r, messageTypeTelemetryAck, ta)
-			if error != nil {
-				log.Fatal(r.Error())
+			err = sendMessage(r, messageTypeTelemetryAck, ta)
+			if err != nil {
+				// TODO retry sending the ACK
+				log.Fatal(err)
 			}
 		default:
 			log.Printf("Unexpected message type: %s", getMessageType(msgType))
--- rfm69.go
+++ rfm69.go
@@ -2,47 +2,58 @@ package main
 
 import (
 	"log"
+	"time"
 
-	"github.com/ecc1/rfm69"
+	"github.com/DanCrank/devices/sx1231"
+	"periph.io/x/periph/conn/gpio"
+	"periph.io/x/periph/conn/gpio/gpioreg"
+	"periph.io/x/periph/conn/spi/spireg"
 )
 
 const frequency uint32 = 915000000
-const bandwidth uint32 = 100000
-const bitrate uint32 = 9600
+const bitrate uint32 = 50000
+const useEncryption bool = true
+const hwDelay time.Duration = 100 * time.Millisecond //generic delay to allow hardware to cope with non-root access
+//see: https://forum.up-community.org/discussion/2141/solved-tutorial-gpio-i2c-spi-access-without-root-permissions
 
-const ackTimeout uint = 1000 // millis to wait for an ack meg
-const msgDelay uint = 100    // millis to wait between Rx and Tx, to give the other side time to switch from Tx to Rx
-const listenDelay uint = 50  // millis to wait between checks of the receive buffer when receiving
-
-func initRadio() *rfm69.Radio {
-	r := rfm69.Open()
-	if r.Error() != nil {
-		log.Fatal(r.Error())
-	}
-	r.Reset()
-	r.InitRF(frequency)
-	r.SetChannelBW(bandwidth)
-	r.SetBitrate(bitrate)
-	dumpRF(r)
-	r.Sleep()
-	return r
+// define a Logprintf function to pass in RadioOpts
+func logPrintf(format string, v ...interface{}) {
+	log.Printf(format, v...)
 }
 
-func dumpRF(r *rfm69.Radio) {
-	if r.Error() != nil {
-		log.Fatal(r.Error())
+func initRadio() *sx1231.Radio {
+	//spiPort, err := spireg.Open("")
+	spiPort, err := spireg.Open("SPI0.0")
+	if err != nil {
+		log.Fatal(err)
 	}
-	log.Printf("Mode: %s", r.State())
-	log.Printf("Frequency: %d Hz", r.Frequency())
-	mod := r.ReadModulationType()
-	switch mod {
-	case rfm69.ModulationTypeFSK:
-		log.Printf("Modulation type: FSK")
-	case rfm69.ModulationTypeOOK:
-		log.Printf("Modulation type: OOK")
-	default:
-		log.Panicf("Unknown modulation mode %X", mod)
+	defer spiPort.Close()
+	time.Sleep(hwDelay)
+
+	interruptPin := gpioreg.ByName("GPIO19")
+	if interruptPin == nil {
+		log.Fatal("Failed to find pin GPIO19 for radio interrupt")
 	}
-	log.Printf("Bitrate: %d baud", r.Bitrate())
-	log.Printf("Channel BW: %d Hz", r.ChannelBW())
+	if err := interruptPin.In(gpio.PullDown, gpio.RisingEdge); err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(hwDelay)
+
+	syncWords := syncWords()
+	radioOpts := sx1231.RadioOpts{
+		Sync:    syncWords[:],
+		Freq:    frequency,
+		Rate:    bitrate,
+		PABoost: true,
+		Logger:  logPrintf,
+	}
+	radio, err := sx1231.New(spiPort, interruptPin, radioOpts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if useEncryption {
+		encryptionKey := encryptionKey()
+		radio.SetEncryptionKey(encryptionKey[:])
+	}
+	return radio
 }
